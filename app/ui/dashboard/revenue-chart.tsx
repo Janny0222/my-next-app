90ca39539b517124018b5b349fcf562f996a1865
Janny0222 <villarueljanmichael@gmail.com>
2024-03-04 16:41:24 +0800

    2nd push

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -14,15 +14,15 @@ export default async function RevenueChart({
 }: {
   revenue: Revenue[];
 }) {
-  const chartHeight = 350;
+  const chartHeight = 250;
   // NOTE: comment in this code when you get to this point in the course
 
-  // const { yAxisLabels, topLabel } = generateYAxis(revenue);
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
-  // if (!revenue || revenue.length === 0) {
-  //   return <p className="mt-4 text-gray-400">No data available.</p>;
-  // }
-
+  if (!revenue || revenue.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  }
+  const maxRevenue = Math.max(...revenue.map((month) => month.revenue));
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -30,36 +30,39 @@ export default async function RevenueChart({
       </h2>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
-          <div
-            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
+      <div className="p-4 rounded-xl bg-gray-50">
+        <div className="grid items-end grid-cols-12 gap-2 p-4 mt-0 bg-white rounded-md sm:grid-cols-13 md:gap-4">
+          
+        {/* <div
+            className="flex-col justify-between hidden mb-6 text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
           >
             {yAxisLabels.map((label) => (
+              <div key={label} className="flex flex-col items-center gap-1">
               <p key={label}>{label}</p>
+              </div>
             ))}
-          </div>
-
+          </div> */}
           {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
+              
               <div
-                className="w-full rounded-md bg-blue-300"
+                className="w-full bg-blue-300 rounded-md"
                 style={{
                   height: `${(chartHeight / topLabel) * month.revenue}px`,
                 }}
               ></div>
-              <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
+              <p className="text-sm text-gray-400">
                 {month.month}
               </p>
             </div>
           ))}
         </div>
-        <div className="flex items-center pb-2 pt-6">
-          <CalendarIcon className="h-5 w-5 text-gray-500" />
+        <div className="flex items-center pt-6 pb-2">
+          <CalendarIcon className="w-5 h-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
